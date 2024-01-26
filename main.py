443d70c16ff5b0d6d353c0ed9bcc67af778ca74d
Skyaner <138205732+Skyaner@users.noreply.github.com>
2024-01-26 15:17:31 +0100

    update

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ def generate_password():
     try:
         length = int(entry_length.get())
     except:
-        Label(root, text="Please enter a Integer for the length of your password.").grid(row=2, column=0)
-        
+        entry_length.insert(0,"Invalid Input: ")
+    
+      
+    
     Digit = ("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
     cletter = ("Q", "W", "E", "R", "T", "Z", "U", "I", "O", "P", "Ü", "A", "S", "D", "F", "G", "H", "J", "K", "L", "Ö", "Ä", "Y", "X", "C", "V", "B", "N", "M")
     letter = ("q", "w", "e", "r", "t", "z", "u", "i", "o", "p", "ü", "a", "s", "d", "f", "g", "h", "j", "k", "l", "ö", "ä", "y", "x", "c", "v", "b", "n", "m")
@@ -14,28 +16,41 @@ def generate_password():
     
     password = ""
     for i in range(length):
-        password += random.choice(Digit + cletter + letter + symbol)
-            
-    Label(root, text=f"{password}").grid(row=2, column=0)
+        password += random.choice(Digit + cletter + letter + symbol)           
+    
+    
+    entry_password = Entry(root)
+    entry_password.insert(0,password)
+    entry_password.config(state="read")
+    entry_password.grid(row=2,column=1)
+    
+    return entry_password
+   
+    
+def delet():
+    entry_length.delete(0,END)   
     
 
 root = Tk()
-root.geometry("400x200")
+root.geometry("500x200")
 root.title("Password Generator")
 root.resizable(0,0)
 
 
 #Description
-Label(root, text="This programm will generat a passwort for you.").grid(row=0)
+Label(root, text="This programm will generate a passwort for you.").grid(row=0)
 
 
 Label(root, text="Please enter a length for your password.").grid(row=1, column=0)
 entry_length = Entry(root)
 entry_length.grid(row=1, column=1)
 
+Label(root, text="Password").grid(row=2, column=0)
 
 Button(root, text="Quit", command=root.quit).grid(row=5, column=0)
-Button(root, text="generate", command=generate_password).grid(row=5, column=1)
+Button(root, text="Generate", command=generate_password).grid(row=5, column=1)
+Button(root, text="Delete", command=delet).grid(row=5, column=2)
+
 
 
 root.mainloop()
